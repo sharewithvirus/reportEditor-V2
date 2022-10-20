20fea03f35fd91e12428949a394445503b4d8e4b
Vaibhav <sharewithvirus@outlook.com>
2022-10-20 13:55:11 +0530

    Added Database Configuration.

--- reportEditor-api/app.js
+++ reportEditor-api/app.js
@@ -5,25 +5,23 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { dbConnect } = require("./config/dbConfig")
 
 var app = express();
-console.log(process.env.DATABASE);
+console.log(`GMI Backend is Listing On Port ${process.env.PORT}`);
+dbConnect()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('combined'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => res.status(200).send("Server Is Running"));
 app.use('/api/v1', require("./routes/1V-main_routes"));
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
 
 // Others Routes
 app.get("*", (req, res) => {
@@ -64,7 +62,7 @@ app.put("*", (req, res) => {
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : console.log(`GMI Editor Backend Running On Port 9090`);
+  res.locals.error = req.app.get('env') === 'development' ? err : console.log(err);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
