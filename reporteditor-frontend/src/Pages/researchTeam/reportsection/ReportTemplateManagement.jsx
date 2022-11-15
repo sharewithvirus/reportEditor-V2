ebdc42aa0c87925152aaec76b3b79f0f1566b1cd
Javed Akhtar <javedakhtar925@gmail.com>
2022-11-15 17:24:12 +0530

    change little design of report-template

--- reporteditor-frontend/src/Pages/researchTeam/reportsection/ReportTemplateManagement.jsx
+++ reporteditor-frontend/src/Pages/researchTeam/reportsection/ReportTemplateManagement.jsx
@@ -1,7 +1,9 @@
-import { Button, Divider, Paper, Typography } from "@mui/material";
+import { Button, Divider, IconButton, Paper, Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import React from "react";
 import FileCopyOutlinedIcon from "@mui/icons-material/FileCopyOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import DoneOutlineOutlinedIcon from '@mui/icons-material/DoneOutlineOutlined';
 function ReportTemplateManagement() {
   return (
     <>
@@ -30,6 +32,24 @@ function ReportTemplateManagement() {
               Report Template Management
             </Typography>
           </Stack>
+          <Stack
+            display="flex"
+            direction="row"
+            justifyContent="start"
+            alignItems="center"
+            spacing={5}
+          >
+            <Button color="inherit"
+            sx={{
+                height:"5vh"
+            }}
+            >
+                Add
+            <IconButton>
+                <AddCircleOutlineOutlinedIcon/>
+            </IconButton>
+                </Button>
+          </Stack>
         </Stack>
         <Divider />
         <Box
@@ -37,8 +57,8 @@ function ReportTemplateManagement() {
             display: "flex",
             justifyContent: "space-between",
             alignItems: "centet",
-           
-            flexWrap:"wrap"
+
+            flexWrap: "wrap",
           }}
         >
           <Box
@@ -47,8 +67,100 @@ function ReportTemplateManagement() {
               justifyContent: "center",
               alignItems: "center",
               flexDirection: "row",
-              minWidth:"500px",
-              marginTop:"100px"
+              minWidth: "500px",
+              marginTop: "100px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                "& > :not(style)": {
+                  m: 1,
+                  width: 160,
+                  height: 180,
+                },
+                marginRight: "100px",
+              }}
+            >
+              <Paper elevation={3} square 
+              sx={{
+                display:"flex",
+                flexDirection:"column",
+                justifyContent:'space-around',
+                alignItems:"center"
+              }}
+              >
+                <Typography sx={{
+                    fontSize:"15px"
+                }}>
+                    Template One
+                </Typography>
+                <IconButton sx={{
+                    fontSize:"10px",
+                }}>
+                <DoneOutlineOutlinedIcon sx={{
+                    fontSize:"15px",
+                    marginRight:"12px"
+                }}/>
+
+                Default
+                </IconButton>
+              </Paper>
+            </Box>
+            <Box
+              sx={{
+                width: "10vw",
+
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  width: "100%",
+                  height: "4vh",
+                  marginTop: "10px",
+                }}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  width: "100%",
+                  height: "4vh",
+                  marginTop: "10px",
+                }}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  width: "100%",
+                  height: "4vh",
+                  marginTop: "10px",
+                }}
+              >
+                Set as Default
+              </Button>
+            </Box>
+          </Box>
+          {/* <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+              minWidth: "500px",
+              marginTop: "100px",
             }}
           >
             <Box
@@ -116,8 +228,8 @@ function ReportTemplateManagement() {
               justifyContent: "center",
               alignItems: "center",
               flexDirection: "row",
-              minWidth:"500px",
-              marginTop:"100px"
+              minWidth: "500px",
+              marginTop: "100px",
             }}
           >
             <Box
@@ -178,15 +290,15 @@ function ReportTemplateManagement() {
                 Set as Default
               </Button>
             </Box>
-          </Box>
+          </Box> */}
           <Box
             sx={{
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
               flexDirection: "row",
-              minWidth:"500px",
-              marginTop:"100px"
+              minWidth: "500px",
+              marginTop: "100px",
             }}
           >
             <Box
@@ -195,13 +307,110 @@ function ReportTemplateManagement() {
                 flexWrap: "wrap",
                 "& > :not(style)": {
                   m: 1,
-                  width: 132,
-                  height: 150,
+                  width: 160,
+                  height: 180,
                 },
                 marginRight: "100px",
               }}
             >
-              <Paper elevation={3} square />
+              <Paper elevation={3} square 
+              sx={{
+                display:"flex",
+                flexDirection:"column",
+                justifyContent:'space-around',
+                alignItems:"center"
+              }}
+              >
+                <Typography sx={{
+                    fontSize:"15px"
+                }}>
+                    Template Two
+                </Typography>
+                
+              </Paper>
+            </Box>
+            <Box
+              sx={{
+                width: "10vw",
+
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  width: "100%",
+                  height: "4vh",
+                  marginTop: "10px",
+                }}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  width: "100%",
+                  height: "4vh",
+                  marginTop: "10px",
+                }}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outlined"
+                color="inherit"
+                sx={{
+                  width: "100%",
+                  height: "4vh",
+                  marginTop: "10px",
+                }}
+              >
+                Set as Default
+              </Button>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+              minWidth: "500px",
+              marginTop: "100px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                "& > :not(style)": {
+                  m: 1,
+                  width: 160,
+                  height: 180,
+                },
+                marginRight: "100px",
+              }}
+            >
+              <Paper elevation={3} square 
+              sx={{
+                display:"flex",
+                flexDirection:"column",
+                justifyContent:'space-around',
+                alignItems:"center"
+              }}
+              >
+                <Typography sx={{
+                    fontSize:"15px"
+                }}>
+                    Template Three
+                </Typography>
+                
+              </Paper>
             </Box>
             <Box
               sx={{
