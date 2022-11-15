ae5cf9aa8721c43e1b8f2ff4c686d5f142fa52eb
Vaibhav <sharewithvirus@outlook.com>
2022-11-15 15:34:53 +0530

    Last Commit At 15-Nov

--- reporteditor-frontend/src/App.js
+++ reporteditor-frontend/src/App.js
@@ -10,20 +10,14 @@ import { userDashboard } from "./Services/authService";
 import "./App.css";
 
 // Import Components
-import DashboardAdmin from "./Pages/superAdmin/dashboard/DashboardAdmin";
 
 import LogIn from "./Pages/login/LogIn";
 import VerifyPassword from "./Pages/verifyPassword/VerifyPassword";
 import Loader from "./components/Loader";
 import DashboardRoutes from "./Pages/Routes/DashboardRoutes";
-import Dashboard from "./Pages/researchTeam/dashboard/Dashboard";
-
-import NavBar from "./components/NavBar";
-import CreateReport from "./Pages/researchTeam/reportsection/component/CreateReport";
 
 function App() {
-  const { isAuthenticated, setIsAuthenticated, setIsLoading, updateAdminData } =
-    useContext(UserDataContext);
+  const { isAuthenticated, setIsAuthenticated, setIsLoading, updateAdminData, setIsAdmin, setUserRole, setUserInfo } = useContext(UserDataContext);
 
   const navigate = useNavigate();
 
@@ -36,6 +30,9 @@ function App() {
     } else {
       setIsAuthenticated(true);
       updateAdminData(res.data.user);
+      setIsAdmin(res.data.data.userData.isAdmin);
+      setUserRole(res.data.data.userData.role);
+      setUserInfo(res.data.data.userData);
       setIsLoading(false);
       if (res.data.user.role === "admin") {
         navigate("/a_control");
@@ -51,31 +48,18 @@ function App() {
   return (
     <>
       <Loader />
-      {/* <CreateReport   /> */}
-
-      { isAuthenticated ? (
+    { isAuthenticated ? (
         <DashboardRoutes />
     )
     :
     (
     <Routes>
-      <Route
-            path="/" 
-            element={<Navigate replace to="login" />}
-          ></Route>
+      <Route path="/" element={<Navigate replace to="login" />} />
       <Route path="/login" element={<LogIn />} />
       <Route path="/verifyEmail/:token" element={<VerifyPassword />} />
     </Routes>
     )
     }
-        {/* <NavBar reportsDashboard={"Research Team Mode"} />
-
-      <Routes>
-        <Route path="/" element={<Navigate replace to="dashboard" />}></Route>
-        <Route path="/dashboard" element={<Dashboard />} />
-         
-        <Route path="create-report" element={ <CreateReport />} />
-      </Routes> */}
     </>
   );
 }
