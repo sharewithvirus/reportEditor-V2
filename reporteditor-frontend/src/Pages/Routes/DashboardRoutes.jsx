6246fcb243a2f43487b1cd9a21392b2699c9f162
Javed Akhtar <javedakhtar925@gmail.com>
2022-12-02 16:42:00 +0530

    little changei

--- reporteditor-frontend/src/Pages/Routes/DashboardRoutes.jsx
+++ reporteditor-frontend/src/Pages/Routes/DashboardRoutes.jsx
@@ -34,10 +34,10 @@ function DashboardRoutes() {
             </Route> */}
 
             <Route path='/u_control'>
-              <Route index element={<ReportEditor />} />
+              {/* <Route index element={<ReportEditor />} /> */}
               {/* <Route index element={<ReportPreview/>}/> */}
               {/* <Route index element={<CreateReport />} /> */}
-              {/* <Route index element={<EditingDashboard/>} /> */}
+              <Route index element={<EditingDashboard/>} />
             {/* <Route index element={<DashboardResearchPanel />} /> */}
               {/* <Route index element={<DraftAccept/>}/> */}
               <Route path="/u_control/create-report" element={<CreateReport />} />
