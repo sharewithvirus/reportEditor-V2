fb50f318634423d7b5bd848c02e92a79e5ea246b
VedobiRohit <rohit.yadav@vedobi.com>
2022-12-15 17:17:44 +0530

    charts new logic

---
+++ reporteditor-frontend/src/Pages/researchTeam/reportEditor/component/Charts/ChartFormGen.jsx
@@ -0,0 +1,82 @@
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { IconButton, TextField } from "@mui/material";
+import React, { useEffect } from "react";
+import GenChart from "./GenChart";
+
+const ChartFormGen = ({
+  setChartFormValues,
+  chartFormValues,
+  saveChartsData,
+  chartType,
+  formChartData,
+  setFormChartData,
+  show,
+  setShow,
+}) => {
+  const handleFormValueChange = (e) => {
+    setShow(false);
+    console.log("formChartDataName", formChartData)
+    setFormChartData({ ...formChartData, [e.target.name]: e.target.value });
+  };
+  const addSeriesField = () => {
+    setChartFormValues([
+      ...chartFormValues,
+      `series${chartFormValues.length - 1}`,
+    ]);
+    console.log("Add Field2", chartFormValues);
+  };
+
+  useEffect(() => {
+    console.log("formChartData", formChartData);
+  }, [formChartData, setFormChartData]);
+
+  console.log("chartFormValues", chartFormValues);
+  return (
+    <>
+      {chartFormValues.map((item, index) => {
+        return (
+          <div key={index} style={{ marginTop: "10px" }}>
+            <TextField
+              id={chartType}
+              label={item}
+              name={item}
+              variant="outlined"
+              onChange={(e) => handleFormValueChange(e)}
+              value={
+                item === "series"
+                  ? formChartData.series
+                  : item === "label"
+                  ? formChartData.label
+                  : item === "series1"
+                  ? formChartData.series1
+                  : item === "series2"
+                  ? formChartData.series2
+                  : item === "categories"
+                  ? formChartData.categories
+                  : ""
+              }
+            />
+
+            {item === "series" && chartType === "bar" ? (
+              <IconButton aria-label="add" onClick={addSeriesField}>
+                <AddCircleIcon />
+              </IconButton>
+            ) : (
+              ""
+            )}
+          </div>
+        );
+      })}
+      <GenChart
+        formChartData={formChartData}
+        setFormChartData={setFormChartData}
+        show={show}
+        setShow={setShow}
+        chartType={chartType}
+        saveChartsData={() => saveChartsData()}
+      />
+    </>
+  );
+};
+
+export default ChartFormGen;
